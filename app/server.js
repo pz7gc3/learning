db28eb5aa77319c991211b77809f35eabc679d95
Johan Sundström <Johan.Sundstrom@iki.fi>
2015-11-27 23:50:16 +0200

    Implemented an empty app/server.js skeleton

---
+++ app/server.js
@@ -0,0 +1,13 @@
+var express = require("express");
+var app = express();
+var converter = require("./converter");
+
+app.get("/rgbToHex", function(req, res) {
+
+});
+
+app.get("/hexToRgb", function(req, res) {
+
+});
+
+app.listen(3000);
