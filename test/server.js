cb6a7e95b807875e1b73e8d473199499effddb44
Johan Sundström <Johan.Sundstrom@iki.fi>
2015-11-27 22:31:31 +0200

    Implements an empty skeleton for testing the server

---
+++ test/server.js
@@ -0,0 +1,22 @@
+var expect  = require("chai").expect;
+var request = require("request");
+
+describe("Color Code Converter API", function() {
+
+  describe("RGB to Hex conversion", function() {
+
+    it("returns status 200", function() {});
+
+    it("returns the color in hex", function() {});
+
+  });
+
+  describe("Hex to RGB conversion", function() {
+
+    it("returns status 200", function() {});
+
+    it("returns the color in RGB", function() {});
+
+  });
+
+});
