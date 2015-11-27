ea756967cdfb8f5d437144c0dd8fd65bb649cd76
Johan Sundström <Johan.Sundstrom@iki.fi>
2015-11-27 23:25:58 +0200

    Now the tests fail as they should

--- test/server.js
+++ test/server.js
@@ -6,15 +6,17 @@ describe("Color Code Converter API", function() {
   describe("RGB to Hex conversion", function() {
     var url = "http://localhost:3000/rgbToHex?red=255&green=255&blue=255";
 
-    it("returns status 200", function() {
+    it("returns status 200", function(done) {
       request(url, function(error, response, body) {
         expect(response.statusCode).to.equal(200);
+        done();
       });
     });
 
-    it("returns the color in hex", function() {
+    it("returns the color in hex", function(done) {
       request(url, function(error, response, body) {
         expect(body).to.equal("ffffff");
+        done();
       });
     });
   });
@@ -22,15 +24,17 @@ describe("Color Code Converter API", function() {
   describe("Hex to RGB conversion", function() {
     var url = "http://localhost:3000/hexToRgb?hex=00ff00";
 
-    it("returns status 200", function() {
+    it("returns status 200", function(done) {
       request(url, function(error, response, body) {
         expect(response.statusCode).to.equal(200);
+        done();
       });
     });
 
-    it("returns the color in RGB", function() {
+    it("returns the color in RGB", function(done) {
       request(url, function(error, response, body) {
         expect(body).to.equal("[0,255,0]");
+        done();
       });      
     });
 
